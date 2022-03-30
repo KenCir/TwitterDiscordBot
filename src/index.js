2fc3cfc476c78c85f4d0832aaa0598c8a170473f
Ken_Cir0909 <65713982+KenCir@users.noreply.github.com>
2022-03-30 12:36:38 +0900

    修正と変更

--- src/index.js
+++ src/index.js
@@ -31,8 +31,13 @@ twitter.stream('statuses/filter', { follow: Object.keys(config.follows).join(','
     stream.on('data', async function (event) {
         if ((!config.mention && event.text.startsWith('@')) || (!config.retweet && event.text.startsWith('RT')) || (config.retweet && !Object.keys(config.follows).includes(event.user.id_str)) || (!config.quotedreTweet && event.quoted_status_id_str) || config.blacklist.includes(event.user.id_str)) return;
 
+        let id_str = event.id_str;
+        if (event.text.startsWith('RT')) {
+            id_str = event.retweeted_status.id_str;
+        }
+
         const msg = await client.channels.cache.get(process.env.DISCORD_CHANNELID).send({
-            content: `${event.user.name}の新規ツイートです\nhttps://twitter.com/${event.user.screen_name}/status/${event.id_str}`,
+            content: `${event.user.name}の新規ツイートです\nhttps://twitter.com/${event.user.screen_name}/status/${id_str}`,
             components: [
                 new MessageActionRow()
                     .addComponents(
@@ -57,7 +62,7 @@ twitter.stream('statuses/filter', { follow: Object.keys(config.follows).join(','
             if (interaction.customId === 'ok') {
                 for (const follow in config.follows) {
                     if (event.user.id_str === follow) {
-                        client.channels.cache.get(config.follows[follow]).send(`${event.user.name}の新規ツイートです\nhttps://twitter.com/${event.user.screen_name}/status/${event.id_str}`);
+                        client.channels.cache.get(config.follows[follow]).send(`${event.user.name}の新規ツイートです\nhttps://twitter.com/${event.user.screen_name}/status/${id_str}`);
                         msg.delete();
                         return;
                     }
@@ -79,47 +84,53 @@ twitter.stream('statuses/filter', { follow: Object.keys(config.follows).join(','
 client.on('messageCreate', async message => {
     if (message.system || !message.guildId || message.author.id !== process.env.DISCORD_USERID || message.channelId !== process.env.DISCORD_CHANNELID) return;
 
-    const buttons = new MessageActionRow()
-        .addComponents(
-            [
-                new MessageButton()
-                    .setCustomId('ok')
-                    .setEmoji('881574101041442887')
-                    .setStyle('PRIMARY'),
-                new MessageButton()
-                    .setCustomId('no')
-                    .setEmoji('881574101444083742')
-                    .setStyle('PRIMARY'),
-            ],
-        );
+    if (message.author.id === process.env.DISCORD_USERID || message.channelId === process.env.DISCORD_CHANNELID) {
+        const buttons = new MessageActionRow()
+            .addComponents(
+                [
+                    new MessageButton()
+                        .setCustomId('ok')
+                        .setEmoji('881574101041442887')
+                        .setStyle('PRIMARY'),
+                    new MessageButton()
+                        .setCustomId('no')
+                        .setEmoji('881574101444083742')
+                        .setStyle('PRIMARY'),
+                ],
+            );
 
-    const msg = await message.reply({
-        content: 'ツイートを送信します、よろしいですか？',
-        components: [buttons],
-    });
+        const msg = await message.reply({
+            content: 'ツイートを送信します、よろしいですか？',
+            components: [buttons],
+        });
 
-    const filter = (interaction) => {
-        return (interaction.customId === 'ok' || interaction.customId === 'no') && interaction.user.id === message.author.id;
-    };
+        const filter = (interaction) => {
+            return (interaction.customId === 'ok' || interaction.customId === 'no') && interaction.user.id === message.author.id;
+        };
 
-    const collector = msg.createMessageComponentCollector({ filter, componentType: 'BUTTON' });
-    collector.on('collect', async (interaction) => {
-        if (interaction.customId === 'ok') {
-            const tweet = await twitter.post('statuses/update', { status: message.content });
-            await interaction.update({
-                content: `ツイートを送信しました\nhttps://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`,
-                components: [],
-            });
-        }
-        else {
-            await interaction.update({
-                content: 'ツイート送信をキャンセルしました',
-                components: [],
-            });
-        }
+        const collector = msg.createMessageComponentCollector({ filter, componentType: 'BUTTON' });
+        collector.on('collect', async (interaction) => {
+            if (interaction.customId === 'ok') {
+                const tweet = await twitter.post('statuses/update', { status: message.content });
+                await interaction.update({
+                    content: `ツイートを送信しました\nhttps://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`,
+                    components: [],
+                });
+            }
+            else {
+                await interaction.update({
+                    content: 'ツイート送信をキャンセルしました',
+                    components: [],
+                });
+            }
 
-        collector.stop();
-    });
+            collector.stop();
+        });
+    }
+
+    if (message.content.startsWith('?twrestart')) {
+        process.exit();
+    }
 });
 
 process.on('unhandledRejection', error => {
