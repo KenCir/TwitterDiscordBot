60ce290818bdb09d5bb4f8c72b5d144e82ebd4fb
Ken_Cir0909 <65713982+KenCir@users.noreply.github.com>
2022-08-17 16:02:48 +0900

    一旦webhookの一方通行方式にする

--- src/index.js
+++ src/index.js
@@ -1,30 +1,25 @@
 require('dotenv').config();
-const { Client, Intents, MessageActionRow, MessageButton } = require('discord.js');
+const { WebhookClient } = require('discord.js');
 const Twitter = require('twitter');
 const config = require('../config.json');
+/*
 const client = new Client({
     intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_INTEGRATIONS,
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildIntegrations,
     ],
     allowedMentions: {
         parse: [],
         repliedUser: false,
     },
 });
+*/
 
 const twitter = new Twitter({
     consumer_key: process.env.TWITTER_KET,
     consumer_secret: process.env.TWITTER_SECRET,
     access_token_key: process.env.TWITTER_ACCESS_TOKEN,
     access_token_secret: process.env.TWITTER_ACCESS_SECRET,
-});
-
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`);
-    client.user.setActivity('Twitter');
-    client.user.setStatus('idle');
 });
 
 twitter.stream('statuses/filter', { follow: Object.keys(config.follows).join(',') }, function (stream) {
@@ -38,24 +33,28 @@ twitter.stream('statuses/filter', { follow: Object.keys(config.follows).join(','
             rt = true;
         }
 
+        const webhook = new WebhookClient({ url: process.env.WEBHOOK_URL });
+        await webhook.send(`${event.user.name}の新規${rt ? 'リ' : ''}ツイートです\nhttps://twitter.com/${event.user.screen_name}/status/${id_str}`);
+        /*
         const msg = await client.channels.cache.get(process.env.DISCORD_CHANNELID).send({
             content: `${event.user.name}の新規${rt ? 'リ' : ''}ツイートです\nhttps://twitter.com/${event.user.screen_name}/status/${id_str}`,
             components: [
-                new MessageActionRow()
+                new ActionRowBuilder()
                     .addComponents(
                         [
-                            new MessageButton()
+                            new ButtonBuilder()
                                 .setCustomId('ok')
                                 .setEmoji('881574101041442887')
-                                .setStyle('PRIMARY'),
-                            new MessageButton()
+                                .setStyle(ButtonStyle.Primary),
+                            new ButtonBuilder()
                                 .setCustomId('no')
                                 .setEmoji('881574101444083742')
-                                .setStyle('PRIMARY'),
+                                .setStyle(ButtonStyle.Primary),
                         ],
                     ),
             ],
         });
+
         const filter = (interaction) => {
             return (interaction.customId === 'ok' || interaction.customId === 'no') && interaction.user.id === process.env.DISCORD_USERID;
         };
@@ -76,6 +75,7 @@ twitter.stream('statuses/filter', { follow: Object.keys(config.follows).join(','
 
             collector.stop();
         });
+        */
     });
 
     stream.on('error', function (error) {
@@ -84,57 +84,7 @@ twitter.stream('statuses/filter', { follow: Object.keys(config.follows).join(','
     });
 });
 
-client.on('messageCreate', async message => {
-    if (message.system || !message.guildId || message.author.id !== process.env.DISCORD_USERID || message.channelId !== process.env.DISCORD_CHANNELID) return;
-
-    if (message.author.id === process.env.DISCORD_USERID || message.channelId === process.env.DISCORD_CHANNELID) {
-        const buttons = new MessageActionRow()
-            .addComponents(
-                [
-                    new MessageButton()
-                        .setCustomId('ok')
-                        .setEmoji('881574101041442887')
-                        .setStyle('PRIMARY'),
-                    new MessageButton()
-                        .setCustomId('no')
-                        .setEmoji('881574101444083742')
-                        .setStyle('PRIMARY'),
-                ],
-            );
-
-        const msg = await message.reply({
-            content: 'ツイートを送信します、よろしいですか？',
-            components: [buttons],
-        });
-
-        const filter = (interaction) => {
-            return (interaction.customId === 'ok' || interaction.customId === 'no') && interaction.user.id === message.author.id;
-        };
-
-        const collector = msg.createMessageComponentCollector({ filter, componentType: 'BUTTON' });
-        collector.on('collect', async (interaction) => {
-            if (interaction.customId === 'ok') {
-                const tweet = await twitter.post('statuses/update', { status: message.content });
-                await interaction.update({
-                    content: `ツイートを送信しました\nhttps://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`,
-                    components: [],
-                });
-            }
-            else {
-                await interaction.update({
-                    content: 'ツイート送信をキャンセルしました',
-                    components: [],
-                });
-            }
-
-            collector.stop();
-        });
-    }
-});
-
 process.on('unhandledRejection', error => {
     console.error(error);
     process.exit();
 });
-
-client.login();
